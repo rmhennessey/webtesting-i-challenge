d5ad8555d71fd7fd2f9156fa24dbaf03ad8dc374
RMH <rory.hennessey@gmail.com>
2019-04-08 17:50:53 -0400

    mvp complete as far as i can test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,44 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  // get,
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return item.enhancement =+ 1
+  } else {
+    return item.enhancement;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return item.durability =- 5
+  } if (item.enhancement > 16) {
+    return item.durability =-10 && item.enhancement--
+  } if (item.enhancement === 15 || item.enhancement === 16) {
+    return item.durability =- 10
+  } if (item.enhancement === 20 ) {
+    return item
+  }
 }
 
 function repair(item) {
-  return { ...item };
+
+  if (item.durability < 100 ) {
+    return item.durability === 100
+  } else {
+    return {...item};
+  }
 }
 
-function get(item) {
-  return { ...item };
-}
+
+//get is stretch. go fish.
+// function get(item) {
+//   if (item.enhancement === 0) {
+//     return item
+//   } if (item.enhancement < 0) {
+//     return `[+${item.enhancement}] ${item}`
+//   }
+// 
